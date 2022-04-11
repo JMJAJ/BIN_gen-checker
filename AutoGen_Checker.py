ac7a499aa9d4e4db5ae97e05ff18c59584b198f8
JMJAJ <34998292+JMJAJ@users.noreply.github.com>
2022-04-11 21:29:18 +0200

    AutoGen_Checker_v4.py
    v4 works complety fine but without saving Live CC into the file

--- AutoGen_Checker.py
+++ AutoGen_Checker.py
@@ -17,16 +17,12 @@ bin_list = ['417849xxxxxxxxxx', '440393xxxxxxxxxx', '520737xxxxxxxxxx', '526879x
           '513483xxxxxxxxxx', '404942xxxxxxxxxx', '527680xxxxxxxxxx', '527684xxxxxxxxxx', '525500xxxxxxxxxx']
 new_bin = []
 
-def Program():
-    for i in range(10):
-        new_bin.append(bin_list[i])
-        gen()
-
 def gen():
     url_gen = "https://technmind.com/ccgen/"
     url_check = "https://www.mrchecker.net/card-checker/ccn2/" 
              ### https://checker.visatk.com/ccn1/
              ### https://www.ontools.net/2022/04/cc-checker.html
+    url_lookup = "https://binlist.net"
 
     bin = "5438164124xxxxxx"
     act = ActionChains(driver)
@@ -49,23 +45,45 @@ def gen():
     time.sleep(1)
     # driver.find_element_by_id("submit").click()
     driver.find_element(By.NAME,"valid").click()
-    time.sleep(30)
+    time.sleep(60)
     # element_to_store = driver.find_element_by_class_name("panel-body").text()
     # print(element_to_store)
 
     ######################## X Not working part X ########################
-    # content = driver.find_elements_by_xpath('//span[@class="rankingItem-value js-countable"]')[0].text
-    content = str.driver.find_elements(By.CSS_SELECTOR('div[@class="panel-body success"]'))
-    act.key_down(Keys.CONTROL).send_keys("c").key_up(Keys.CONTROL).perform()
+    driver.find_element(By.CSS_SELECTOR, ".panel-body.success")
+    content = act.key_down(Keys.CONTROL).send_keys("c").key_up(Keys.CONTROL).perform()
     print(content)
     ######################## X Not working part X ########################
 
     ### Third Part ###
     # path = r'C:\Users\uzivatel 1\source\repos\AutoGen+Checker\AutoGen+Checker\Live_check.txt'
     file = open('Live_check.txt', 'a')
-    file.write(str(content) + "\n")
+    file.write(str(content) + '\n')
     file.close()
+    
+
+    ### Bonus Part ###
+    def virtualCards():
+        driver.get(url_lookup)
+        bin_list_2 = [417849, 440393, 520737, 526879, 409602, 513483,
+                    526879, 409602, 513483, 404942, 527680, 527684, 525500]
+        i = 0
+        while (i < int(max(bin_list_2))):
+            num = int(bin_list_2[i])
+            num_str = str(num)
+            eDigit = num_str[:8]
+            driver.find_element(By.CLASS_NAME, "iin").click()
+            act.key_down(Keys.CONTROL).send_keys("a").key_up(Keys.CONTROL).perform()
+            # driver.find_element_by_name("iin").send_keys(eDigit)
+            driver.find_element(By.CSS_SELECTOR, "input[value='4571 7360']").send_keys(eDigit)
+            if (num == int(max(bin_list_2))):
+                print("It works")
+                exit(0)
+            i = i + 1
+            time.sleep(1)
+
+    virtualCards()
 
     time.sleep(2)
 
-Program()
+gen()
