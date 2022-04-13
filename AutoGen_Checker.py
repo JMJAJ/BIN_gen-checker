c5808c6a9ab9e6e40bd174a42923db930c38851d
JMJAJ <34998292+JMJAJ@users.noreply.github.com>
2022-04-14 00:09:38 +0200

    AutoGen_Checker_v5.py

--- AutoGen_Checker.py
+++ AutoGen_Checker.py
@@ -7,21 +7,21 @@ from undetected_chromedriver import Chrome
 import undetected_chromedriver.v2 as uc
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver = Chrome()
 driver = uc.Chrome(use_subprocess=True)
 
 import time
 
 
 bin_list = ['417849xxxxxxxxxx', '440393xxxxxxxxxx', '520737xxxxxxxxxx', '526879xxxxxxxxxx', '409602xxxxxxxxxx',
-          '513483xxxxxxxxxx', '404942xxxxxxxxxx', '527680xxxxxxxxxx', '527684xxxxxxxxxx', '525500xxxxxxxxxx']
+          '513483xxxxxxxxxx', '404942xxxxxxxxxx', '527680xxxxxxxxxx', '527684xxxxxxxxxx', '525500xxxxxxxxxx',
+          '414718xxxxxxxxxx']
 new_bin = []
 
 def gen():
     url_gen = "https://technmind.com/ccgen/"
-    url_check = "https://www.mrchecker.net/card-checker/ccn2/" 
-             ### https://checker.visatk.com/ccn1/
-             ### https://www.ontools.net/2022/04/cc-checker.html
+    url_check_a = "https://www.mrchecker.net/card-checker/ccn2/" 
+    url_check_b = "https://checker.visatk.com/ccn1/"
+    ############# "https://www.ontools.net/2022/04/cc-checker.html"
     url_lookup = "https://binlist.net"
 
     bin = "5438164124xxxxxx"
@@ -34,55 +34,44 @@ def gen():
     act.key_down(Keys.CONTROL).send_keys("a").key_up(Keys.CONTROL).perform()
     driver.find_element_by_name("ccghm").send_keys(100)
     driver.find_element_by_id("generar").click()
-    # driver.find_element_by_class_name("copytoclipboard_data").click()
     driver.find_element(By.CLASS_NAME, "copytoclipboard_data").click()
     
-    ### Second Part ###
-    driver.get(url_check)
-    time.sleep(7)
-    driver.find_element_by_class_name("form-control").click()
-    act.key_down(Keys.CONTROL).send_keys("v").key_up(Keys.CONTROL).perform()
-    time.sleep(1)
-    # driver.find_element_by_id("submit").click()
-    driver.find_element(By.NAME,"valid").click()
-    time.sleep(60)
-    # element_to_store = driver.find_element_by_class_name("panel-body").text()
-    # print(element_to_store)
+    print("With CloudFlare - 1\nWithout CloudFlare - 2")
+    if (switch == 1):
+        ### Second Part (A) ###
+        driver.get(url_check_a)
+        time.sleep(7)
+        driver.find_element_by_class_name("form-control").click()
+        act.key_down(Keys.CONTROL).send_keys("v").key_up(Keys.CONTROL).perform()
+        time.sleep(1)
+        driver.find_element(By.NAME,"valid").click()
+        time.sleep(60)
+        # element_to_store = driver.find_element_by_class_name("panel-body").text()
+        # print(element_to_store)
+    else:                                             
+        ### Second Part (B) ###
+        driver.get(url_check_b)
+        driver.find_element_by_class_name("form-control").click()
+        act.key_down(Keys.CONTROL).send_keys("v").key_up(Keys.CONTROL).perform()
+        time.sleep(1)
+        driver.find_element_by_id("submit").click()
+        time.sleep(60)
+        store = driver.find_element(By.XPATH, "//div[@id='tvmit_live']").text()
 
     ######################## X Not working part X ########################
-    driver.find_element(By.CSS_SELECTOR, ".panel-body.success")
-    content = act.key_down(Keys.CONTROL).send_keys("c").key_up(Keys.CONTROL).perform()
-    print(content)
+    # driver.find_element(By.CSS_SELECTOR, ".panel-body.success")
+    # content = act.key_down(Keys.CONTROL).send_keys("c").key_up(Keys.CONTROL).perform()
+    # print(content)
     ######################## X Not working part X ########################
 
     ### Third Part ###
-    # path = r'C:\Users\uzivatel 1\source\repos\AutoGen+Checker\AutoGen+Checker\Live_check.txt'
     file = open('Live_check.txt', 'a')
     file.write(str(content) + '\n')
     file.close()
     
 
     ### Bonus Part ###
-    def virtualCards():
-        driver.get(url_lookup)
-        bin_list_2 = [417849, 440393, 520737, 526879, 409602, 513483,
-                    526879, 409602, 513483, 404942, 527680, 527684, 525500]
-        i = 0
-        while (i < int(max(bin_list_2))):
-            num = int(bin_list_2[i])
-            num_str = str(num)
-            eDigit = num_str[:8]
-            driver.find_element(By.CLASS_NAME, "iin").click()
-            act.key_down(Keys.CONTROL).send_keys("a").key_up(Keys.CONTROL).perform()
-            # driver.find_element_by_name("iin").send_keys(eDigit)
-            driver.find_element(By.CSS_SELECTOR, "input[value='4571 7360']").send_keys(eDigit)
-            if (num == int(max(bin_list_2))):
-                print("It works")
-                exit(0)
-            i = i + 1
-            time.sleep(1)
-
-    virtualCards()
+    ### /Exaple2.py ###
 
     time.sleep(2)
 
